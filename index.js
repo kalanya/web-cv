4419348e01324336d3443617ef4d0e250fc2e804
kalanya <auro.miralles.contreras@gmail.com>
2017-10-23 04:49:49 +0200

    updates

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ var $ = require('jquery');
 
 $(document).ready(function(){
 
+
 	function animateBars() {
-		var subjects = {'Git': 70, 'Linux': 80, 'Sistemas': 70, 'Bash': 70, 'Java': 80, 'Python': 85,
-						'JavaScript': 70, 'HTML': 75, 'CSS': 60, 'Redes': 60,
-						'SQL': 80, 'MDX': 65, 'XPath/XQuery': 65, 'Integración de datos': 65};
-    	$('.myBars').each(function(i, elem){
+		var subjects = {'Git': 65, 'Linux': 80, 'Sistemas': 70, 'Bash': 70, 'Java': 80, 'Python': 85,
+						'JavaScript': 70, 'HTML/CSS': 75, 'Redes': 60,
+						'SQL': 80, 'MDX': 75, 'XPath/XQuery': 75, 'Integración de datos': 70,
+						'R': 75, 'Matlab': 70, 'Minería de datos': 65};
+    	$('.bars').each(function(i, elem){
     		var width = 1;
     		var id = setInterval(frame, 10);
     		function frame() {
-        	 	if (width >= subjects[elem.textContent]) {
+        	 	if (width >= subjects[elem.firstChild.textContent]) {
             		clearInterval(id);
         		} else {
             		width++;
