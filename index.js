15d6f207801d4a2358439ad90c9604d400ba693f
kalanya <auro.miralles.contreras@gmail.com>
2017-10-26 17:28:32 +0200

    update

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 
 
 	function animateBars() {
-		var subjects = {'Git': 65, 'Linux': 80, 'Sistemas y redes': 70, 'Bash': 70, 'Java': 80, 'Python': 85,
-						'JavaScript': 70, 'HTML/CSS': 75,
+		var subjects = {'Git': 65, 'Linux': 80, 'Sistemas': 70, 'Bash': 70, 'Java': 80, 'Python': 85,
+						'JavaScript': 70, 'HTML/CSS': 75, 'Redes': 70,
 						'SQL': 80, 'MDX': 75, 'XPath/XQuery': 75, 'Integración de datos': 70,
 						'R': 75, 'Matlab': 70, 'Minería de datos': 65};
     	$('.bars').each(function(i, elem){
